d9e6925a579449b1a3cf8314dbbb1ab0c2d25a2a
ainahid123 <mdarifulislamnahid125108@gmail.com>
2025-09-16 19:50:59 +0600

    server running

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express, { Request, Response } from "express";
+import { Server } from "http";
 
 const app = express();
+const PORT = 5100;
 
 app.get("/", (req: Request, res: Response) => {
-  console.log("Server Running");
+  res.send("Server Running");
+});
+
+const server: Server = app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
