5a9dfed7f3e4c724c95f0c18cbb8dba949155cd7
ainahid123 <mdarifulislamnahid125108@gmail.com>
2025-09-21 16:53:00 +0600

    error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,6 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import httpStatusCode from "http-status-codes";
 import { envVars } from "../config/env";
 import { TErrorSources } from "../interfaces/error.types";
+import { handlerDuplicateError } from "../error/handlerDuplicateError";
+import { handleCastError } from "../error/handleCastError";
+import { handlerZodError } from "../error/handlerZodError";
+import { handlerValidationError } from "../error/handlerValidationError";
+import AppError from "../config/appError";
 
 export const errorHandler = (
   err: any,
@@ -14,6 +19,37 @@ export const errorHandler = (
   let errorSources: TErrorSources[] = [];
   let message: string = "Error Happend";
   let statusCode: number = 500;
+
+  //Duplicate error
+  if (err.code === 11000) {
+    const simplifiedError = handlerDuplicateError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+  // Object ID error / Cast Error
+  else if (err.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  } else if (err.name === "ZodError") {
+    const simplifiedError = handlerZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+  }
+  //Mongoose Validation Error
+  else if (err.name === "ValidationError") {
+    const simplifiedError = handlerValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+    message = simplifiedError.message;
+  } else if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    statusCode = 500;
+    message = err.message;
+  }
 
   res.status(statusCode).json({
     success: false,
