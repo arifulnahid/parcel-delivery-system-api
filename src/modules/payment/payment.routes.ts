b0ee9eaaaebf72349e9565ddf3adb579b189dafd
ainahid123 <mdarifulislamnahid125108@gmail.com>
2025-09-29 22:14:50 +0600

    payment auth

--- src/modules/payment/payment.routes.ts
+++ src/modules/payment/payment.routes.ts
@@ -5,21 +5,43 @@ import {
   makePaymentZodSchema,
   updatePaymentZodSchema,
 } from "./payment.validation";
+import passport from "passport";
+import { Role } from "../user/user.inerface";
+import authorize from "../../middlewares/authorize";
 
 const router = Router();
 
 router.post(
   "/make",
   validateRequest(makePaymentZodSchema),
+  passport.authenticate("jwt", { session: false }),
+  authorize(Role.Admin, Role.Agent, Role.Rider, "AUTH"),
   PaymentControllers.makePayment
 );
 router.patch(
   "/update",
   validateRequest(updatePaymentZodSchema),
+  passport.authenticate("jwt", { session: false }),
+  authorize(Role.Admin, Role.Agent, Role.Rider),
   PaymentControllers.updatePayment
 );
-router.get("/:paymentId", PaymentControllers.getPaymentById);
-router.get("parcel/parcelId", PaymentControllers.getPaymentByParcelId);
-router.delete("/delete/:paymentId", PaymentControllers.deletePayment);
+router.get(
+  "/:paymentId",
+  passport.authenticate("jwt", { session: false }),
+  authorize(Role.Admin, Role.Agent, Role.Rider),
+  PaymentControllers.getPaymentById
+);
+router.get(
+  "parcel/parcelId",
+  passport.authenticate("jwt", { session: false }),
+  authorize(Role.Admin, Role.Agent, Role.Rider),
+  PaymentControllers.getPaymentByParcelId
+);
+router.delete(
+  "/delete/:paymentId",
+  passport.authenticate("jwt", { session: false }),
+  authorize(Role.Admin, Role.Agent),
+  PaymentControllers.deletePayment
+);
 
 export const PaymentRouter = router;
